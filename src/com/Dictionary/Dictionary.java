5c633f8364480fe032693b3c563b967d79de0d73
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-07-27 21:54:14 -0400

    finished the remove method

--- src/com/Dictionary/Dictionary.java
+++ src/com/Dictionary/Dictionary.java
@@ -3,6 +3,24 @@ package com.Dictionary;
 public class Dictionary {
 
     public static void main(String[] args) {
-        
+        String word = "waiting";
+        String definition = "let time pass";
+
+        WordList wordList = new WordList();
+
+        wordList.insert( new WordMeaning(word,definition));
+
+        System.out.println(wordList.toString());
+
+        String newWord = "Love";
+        String newMeaning = "care for someone";
+
+
+        wordList.insert( new WordMeaning(newWord,newMeaning));
+
+        System.out.println(wordList.toString());
+
+        wordList.remove(word);
+        System.out.println(wordList.toString());
     }
 }
--- src/com/Dictionary/WordList.java
+++ src/com/Dictionary/WordList.java
@@ -12,11 +12,63 @@ public class WordList {
         WordMeaningNode current = this.list;
 
         while (current != null){
-            result += current.getWord().getWordName() + " - " + current.getWord().getMeaning();
+            result += current.getWord().getWordName() + " - " + current.getWord().getMeaning() + "\n";
             current = current.next;
         }
         return result;
     }
+
+    public void remove(String word) {
+        if (this.list == null) {
+            System.out.println("there are no words in the dictionary");
+        } else {
+            WordMeaningNode back = null;
+            WordMeaningNode current = this.list;
+            boolean found = false;
+
+            while (current != null && !found) {
+                if (word.equalsIgnoreCase(current.getWord().getWordName())) {
+                    found = true;
+                } else {
+                    back = current;
+                    current = current.next;
+                }
+            }
+            if(back == null) {
+                list = list.next;
+            }
+            else {
+                back.next = current.next;
+            }
+        }
+    }
+
+
+//        WordMeaningNode temp = new WordMeaningNode(word);
+//        if(this.list == null)
+//            System.out.println("there are no words in the dictionary");
+//        else{
+//            WordMeaningNode back = null;
+//            WordMeaningNode current = this.list;
+//            boolean found = false;
+//
+//            while(current != null && !found)
+//            {
+//                if(temp.getWord().getWordName().compareTo(current.getWord().getWordName()) < 0){
+//                    found = true;
+//                }
+//                else
+//                {
+//                    back = current;
+//                    current = current.next;
+//                }
+//            }
+//                }
+//                else
+//                {
+//                    back = current;
+//                    current = current.next;
+//                }
 
     public void insert(WordMeaning word){
         WordMeaningNode temp = new WordMeaningNode(word);
